36fc20d3f37eb7fc7b4aec0f6c04f906e2e86401
Eslam Fayez <118452237+EslamFayez99@users.noreply.github.com>
2023-02-26 18:40:38 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,42 @@
+let toggleBtn = document.querySelector(".navbtn");
+let tLinks = document.querySelector(".navbar");
+let closeNav = document.querySelector(".close");
+
+toggleBtn.onclick = function () {
+
+    tLinks.classList.add("active")
+
+};
+
+closeNav.onclick = () => {
+
+    tLinks.classList.remove('active');
+
+};
+
+
+
+let header = document.getElementById("header");
+
+let i = 800;
+
+
+window.onscroll = function () {
+
+    scrollHeader();
+
+}
+
+function scrollHeader() {
+
+    if(window.scrollY >= i) {
+
+        header.classList.add("bg-header");
+
+    }else{
+
+        header.classList.remove("bg-header");
+
+    }
+
+}
